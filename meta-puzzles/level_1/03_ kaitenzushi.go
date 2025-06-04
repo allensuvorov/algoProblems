629f8f5106f11c42636be45fe4aceea0f01d81f0
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-06-04 16:53:40 -0700

    Update 03_ kaitenzushi.go
    * refactor naming

--- meta-puzzles/level_1/03_ kaitenzushi.go
+++ meta-puzzles/level_1/03_ kaitenzushi.go
@@ -6,22 +6,22 @@ import "strconv"
 import "strings"
 
 func getMaximumEatenDishCount(N int32, D []int32, K int32) int32 {
-  justEaten := make(map[int32]bool)
-  q := make([]int32, 0, K)
-  var dishCount int32
+  set := make(map[int32]bool) // set of previous K eaten
+  q := make([]int32, 0, K) // queue of previous K eaten
+  var res int32
   
   for _, dish := range D {
-    if !justEaten[dish] {
-      dishCount++
-      justEaten[dish] = true
+    if !set[dish] {
+      res++
+      set[dish] = true
       q = append(q, dish) // enqueue
     }
     
     if len(q) > int(K) { // dequeue
-      justEaten[q[0]] = false
+      set[q[0]] = false
       q = q[1:]
     }
     
   }
-  return dishCount
+  return res
 }
